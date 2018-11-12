e5914e4b3cad2c8f01c838a1675e39a6a5ac25a7
Hristo Yankov <ico.yankov@gmail.com>
2018-11-12 20:37:26 +0200

    second pomodoro

--- rust/reversi.rs
+++ rust/reversi.rs
@@ -12,12 +12,12 @@ fn main() {
     print_board(solve(board, player));
 }
 
-fn print_board(board: Board) {
+fn pri8_board(board: Board) {
     for line in 0..board.len() {
         for column in 0..board[line].len() {
-            print!("{} ", board[line][column])
+            pri8!("{} ", board[line][column])
         }
-        println!("")
+        pri8ln!("")
     }
 }
 
@@ -33,7 +33,34 @@ fn solve(board: Board, player: u8) -> Board {
     return board
 }
 
-fn find_move_up(board: Board, line: usize, column: usize) -> (u8, u8) {
+fn find_move_up(board: Board, line: usize, column: usize, direction: (i8, i8)) -> Option<(u8, u8)> {
     let player = board[line][column];
-    return (0, 0)
+    let mut current_pos = (line as i8, column as i8);
+    while true {
+        _pos = (current_pos.0 + dir.0, current_pos.1 + dir.1);
+        if !is_in_bounds(board, current_pos) {
+            return None;
+        }
+    }
+    for x in line - 2..0 {
+        if board[x][column] == player {
+            break;
+        }
+        if board[x][column] == 0 {
+            return Some((x as u8, column as u8));
+        }
+    }
+    return None;
+}
+
+fn is_in_bounds(board: Board, pos: (i8, i8)) -> bool {
+    return pos.0 >= 0 && pos.0 < board.len()
+        && pos.1 >= 0 && pos.1 < board[0].len();
+}
+
+fn opposite_player(player: u8) -> u8 {
+     match player {
+        1 => return 2,
+        _ => return 1
+     }
 }
